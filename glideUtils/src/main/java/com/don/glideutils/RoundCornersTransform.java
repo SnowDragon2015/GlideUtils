a690a90cfe07fb7576593610e5b10971d39b89e1
SnowDragon2015 <chyychfchx@163.com>
2020-11-13 07:42:45 +0800

    1.0.1

--- glideUtils/src/main/java/com/don/glideutils/RoundCornersTransform.java
+++ glideUtils/src/main/java/com/don/glideutils/RoundCornersTransform.java
@@ -44,7 +44,6 @@ public class RoundCornersTransform extends BitmapTransformation {
         this.radius[1] = tr;
         this.radius[2] = br;
         this.radius[3] = bl;
-
     }
 
     @Override
