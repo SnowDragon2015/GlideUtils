250ffb23441cd33ba2336752cc7ee2cd0ce58502
SnowDragon2015 <chyychfchx@163.com>
2020-12-24 08:24:43 +0800

    [新增] branche222实体类

---
+++ app/src/main/java/com/don/comm/test/Annimation.java
@@ -0,0 +1,10 @@
+package com.don.comm.test;
+
+/**
+ * FileName: Annimation
+ * Author: SnowDragon
+ * Date: 2020/12/24 8:23
+ * Description:
+ */
+class Annimation {
+}
---
+++ app/src/main/java/com/don/comm/test/Fruit.java
@@ -0,0 +1,10 @@
+package com.don.comm.test;
+
+/**
+ * FileName: Fruit
+ * Author: SnowDragon
+ * Date: 2020/12/24 8:24
+ * Description:
+ */
+public class Fruit {
+}
