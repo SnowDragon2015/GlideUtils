ba814b54caf09b2dc24a44bb335b39c564654745
SnowDragon2015 <chyychfchx@163.com>
2020-12-24 08:26:55 +0800

    [修改] branche1实体类

--- app/src/main/java/com/don/comm/test/Branch1.java
+++ app/src/main/java/com/don/comm/test/Branch1.java
@@ -7,4 +7,5 @@ package com.don.comm.test;
  * Description:
  */
 public class Branch1 {
+
 }
--- app/src/main/java/com/don/comm/test/Student.java
+++ app/src/main/java/com/don/comm/test/Student.java
@@ -19,6 +19,7 @@ public class Student {
         this.age = age;
     }
 
+
     public String getName(){
         return name;
     }
